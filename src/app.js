d7486f836544c37c773f3f2fa8eb33558b3d4422
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2023-08-24 02:24:52 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function generateDomainName() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big", "dazzling", "small"];
+  const noun = ["jogger", "racoon", "book", "padawan"];
+  const extensions = ["com", "net", "us", "io"];
+
+  const aleatoryPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+
+  const aleatoryAdj = adj[Math.floor(Math.random() * adj.length)];
+
+  const aleatoryNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  const aleatoryExtensions =
+    extensions[Math.floor(Math.random() * extensions.length)];
+
+  console.log(
+    `${aleatoryPronoun}${aleatoryAdj}${aleatoryNoun}.${aleatoryExtensions}`
+  );
 };
